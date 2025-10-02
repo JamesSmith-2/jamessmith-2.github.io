c214a19058c37dc523677f22a4d645dda554e493
JamesSmith-2 <jasm7646@colorado.edu>
2025-10-02 17:40:56 -0600

    wa7 recitation

--- html/week7/wa7.js
+++ html/week7/wa7.js
@@ -10,6 +10,8 @@ navToggle.addEventListener("click", function () {
   // navMenu.classList.toggle('show');
 });
 
+navToggle.addEventListener("click", showMenu);
+
 navMenu.addEventListener("click", function () {
   navMenu.classList.toggle("active");
 });
@@ -50,12 +52,11 @@ feedbackForm.addEventListener("submit", (e) => {
 });
 
 //Light / Dark Mode
-let button = document.querySelector('#theme').addEventListener('click', theme);
+let button = document.querySelector("#theme").addEventListener("click", theme);
 
 function theme() {
-    // console.lot("theme works");
-    setTheme("light");
-
+  // console.log("theme works");
+  setTheme("light");
 }
 
 // // Save user's theme choice
@@ -77,3 +78,46 @@ function theme() {
 //     document.body.className = savedTheme;
 // });
 
+//These are the footer buttons that determine the size of text on the page
+const small_txt = document.querySelector("#small-text");
+const med_txt = document.querySelector("#med-text");
+const large_txt = document.querySelector("#large-text");
+const clear_txt_pref = document.querySelector("#clear-preferences");
+
+function set_text_size() {
+  if(localStorage.getItem("fontSize") !== null) {
+    let temp_size = localStorage.getItem("fontSize");
+    document.querySelector("html").style.fontSize = String(temp_size) + px;
+    
+  }
+}
+
+function change_text_size(size) {
+  let size_in_px = size * 16;
+
+  document.querySelector("html").style.fontSize = String(size_in_px) + "px";
+
+  localStorage.setItem("fontSize", size_in_px);
+
+  console.log("change function");
+}
+
+function clear_local_storage(size) {
+  let size_in_px = size * 16;
+
+  localStorage.clear();
+  document.querySelector("html").style.fontSize = String(size_in_px) + "px";
+}
+
+small_txt.addEventListener("click", () => {
+  change_text_size(0.8);
+});
+med_txt.addEventListener("click", () => {
+  change_text_size(1);
+});
+large_txt.addEventListener("click", () => {
+  change_text_size(1.5);
+});
+clear_txt_pref.addEventListener("click", () => {
+  clear_local_storage(1);
+});
